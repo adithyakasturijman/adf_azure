fda9b60810bf9a43dcb85ac45b224187f362cf28
adithyakasturijman <adithya.k@jmangroup.com>
2025-03-14 19:49:52 +0530

    Delete mini_project directory

--- mini_project/macros/get_prev_revenue.sql
+++
@@ -1,6 +0,0 @@
-{% macro get_prev_revenue(previousdate,CUSTOMER_ID,product_id) -%}
- 
-    select TOTAL_REVENUE from {{ref('join')}}
-    where PAYMENT_MONTH={{previousdate}} AND CUSTOMER_ID={{CUSTOMER_ID}} and product_id={{product_id}}
- 
-{%- endmacro %}
